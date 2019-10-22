085c69ae66fd56f833b36dcff83740ce902389b8
Christian Cain <christian22cain@yahoo.com>
2019-10-21 19:28:16 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO series VALUES 
+(1, "Merlin", 1, 1),
+(2, "Cirque Du Freak", 2, 2);
+
+INSERT INTO subgenres VALUES 
+(1, "Magic"),
+(2, "Vampires");
+
+INSERT INTO authors VALUES
+(1, "T. A. Barron"),
+(2, "Darren Shan");
+
+INSERT INTO books VALUES 
+(1, "The Lost Years of Merlin", 1970, 1),
+(2, "The Seven Songs of Merlin", 1972, 1),
+(3, "The Fired of Merlin", 1974, 1),
+(4, "Cirque du Freak", 1980, 2),
+(5, "The Vampire's Assistant", 1982, 2),
+(6, "Tunnels of Blood", 1984, 2);
+
+INSERT INTO characters VALUES
+(1, "Merlin", "Hocus pocus", "Wizard", 1),
+(2, "Fincayra", "Screetch!", "Dragon", 1),
+(3, "Balor", "Im gone kill you Merlin!", "Demon", 1),
+(4, "Rhita Gawr", "Evil is fun!", "Demon", 1),
+(5, "Darren Shan", "How do you vampire?", "Half-blood", 2),
+(6, "Larten Crepsley", "Lets get it!", "Vampire", 2),
+(7, "Kurda Smahlt", "I don' fight.", "Vampire", 2),
+(8, "Arra Sails", "Women can fight too!", "Martian", 2);
+
+INSERT INTO character_books VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 2),
+(5, 2, 3),
+(6, 3, 3),
+(7, 3, 2),
+(8, 4, 2),
+(9, 5, 4),
+(10, 5, 5),
+(11, 5, 6),
+(12, 6, 4),
+(13, 6, 5),
+(14, 7, 4),
+(15, 7, 5),
+(16, 8, 4);
