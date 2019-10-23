4b65cffa6d0e5a45d171d12b1854b559214482ef
Mars Le <hp.mars.le@gmail.com>
2019-10-23 20:59:35 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year"
+  "SELECT title, year FROM books WHERE series_id = (SELECT MIN(series_id) FROM books) ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  ""
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT species, COUNT(species) AS count FROM characters GROUP BY species ORDER BY count DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
