bc3fe74c5ccea3982299bfb2accf5d326258fe29
Mars Le <hp.mars.le@gmail.com>
2019-10-23 22:12:16 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,13 +12,14 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  ""
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series ON series.author_id = authors.id INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  ""
+  "SELECT series.title FROM series INNER JOIN books ON series.id = books.series_id INNER JOIN character_books ON books.id = character_books.book_id INNER JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title HAVING COUNT(characters.species) ORDER BY characters.species DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  ""
+  "SELECT characters.name" 
+  # INNER JOIN character_books INNER JOIN books 
 end
