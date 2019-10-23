fc61fa98b912a1ff6a38696c68869f0c833a2922
Mars Le <hp.mars.le@gmail.com>
2019-10-23 18:12:02 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,39 +1,37 @@
 CREATE TABLE series (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY, 
   title TEXT, 
   author_id INTEGER, 
   subgenre_id INTEGER
 );
 
-CREATE TABLE subgenres(
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
 
-CREATE TABLE authors(
+CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-  name TEXT, 
+  name TEXT 
 );
 
-
-CREATE TABLE books(
+CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
 );
 
-CREATE TABLE characters(
+CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
-  spcies TEXT,
+  species TEXT,
   author_id INTEGER
 );
 
-CREATE TABLE character_books(
+CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
   book_id INTEGER,
   character_id INTEGER
 );
-
