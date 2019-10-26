cb6a6f19620155c223fde7d2cb8bbb692f4303a6
Kierra M <k.merriman32@hotmail.com>
2019-10-26 00:05:06 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT (*) 
   FROM characters 
   GROUP BY species 
-  ORDER BY COUNT (*) DESC
+  ORDER BY COUNT (*) 
   LIMIT 1;"
 end
 
